9ffb1e987518674d8c25c351e6bb010dbdadf861
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-04-15 16:31:21 -0500

    Login

---
+++ project/lib/View/Widget/MyApp.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:project/View/Screem/LoginPage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      showSemanticsDebugger: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ project/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:project/View/Widget/MyApp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
