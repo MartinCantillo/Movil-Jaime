0062c9fd0056586f3cc7103abb99e3e5de785380
Elchicogel <rodriguezcantillojaimealfonso@gmail.com>
2024-04-18 08:52:02 -0500

    commit

--- project/lib/View/Screem/PagoScreem.dart
+++ project/lib/View/Screem/PagoScreem.dart
@@ -5,7 +5,7 @@ import 'package:project/View/Screem/ReservarScreem.dart';
 import 'package:project/View/Screem/UnirseJuegoScreem.dart';
 
 class PagoScreem extends StatefulWidget {
-  const PagoScreem({super.key});
+  const PagoScreem({Key? key}) : super(key: key);
   static const String nombre = 'PagoScreem';
 
   @override
@@ -13,67 +13,120 @@ class PagoScreem extends StatefulWidget {
 }
 
 class _PagoScreemState extends State<PagoScreem> {
+  bool _processingPayment = false;
+
+  void _processPayment() {
+    // Aquí deberías implementar la lógica para procesar el pago,
+    // ya sea integrando una pasarela de pago como Stripe o PayPal.
+    // Esto puede requerir llamadas a API, etc.
+    setState(() {
+      _processingPayment = true;
+    });
+
+    // Simulación de proceso de pago (espera de 2 segundos)
+    Future.delayed(Duration(seconds: 2), () {
+      // Lógica de confirmación de reserva y notificación al usuario
+
+      // Ejemplo de notificación mediante un diálogo
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Pago completado'),
+            content: Text('Tu pago ha sido procesado con éxito.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cerrar'),
+              ),
+            ],
+          );
+        },
+      );
+
+      setState(() {
+        _processingPayment = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      drawer: Drawer(
-        backgroundColor: Colors.black,
-        shadowColor: Colors.white,
-        child: ListView(padding: EdgeInsets.zero, children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.orange,
-            ),
-            child: Icon(
-              Icons.person,
-              size: 100,
-              color: Colors.black,
-            ),
-          ),
-          ListTile(
-            title: const Text('Home ', style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(MyHomePage.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Crear equipo ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(CEquipoScreem.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Unirse a Juego ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(UnirseJuegoScreem.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Reservar instalaciones ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(ReservarScreem.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Realizar pago ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(PagoScreem.nombre);
-            },
-          ),
-        ]),
-      ),
-      appBar: AppBar(
+      child: Scaffold(
+        appBar: AppBar(
           title: const Text(
             "Realizar pago",
             style: TextStyle(color: Colors.black),
           ),
           centerTitle: true,
-          backgroundColor: Colors.orange),
-    ));
+          backgroundColor: Colors.orange,
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.black,
+          shadowColor: Colors.white,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                ),
+                child: Icon(
+                  Icons.person,
+                  size: 100,
+                  color: Colors.black,
+                ),
+              ),
+              ListTile(
+                title:
+                    const Text('Home ', style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(MyHomePage.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Crear equipo ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(CEquipoScreem.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Unirse a Juego ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(UnirseJuegoScreem.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Reservar instalaciones ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(ReservarScreem.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Realizar pago ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(PagoScreem.nombre);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: _processingPayment
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ElevatedButton(
+                onPressed: _processPayment,
+                child: Text('Pagar'),
+              ),
+      ),
+    );
   }
 }
--- project/lib/View/Screem/UnirseJuegoScreem.dart
+++ project/lib/View/Screem/UnirseJuegoScreem.dart
@@ -5,73 +5,128 @@ import 'package:project/View/Screem/PagoScreem.dart';
 import 'package:project/View/Screem/ReservarScreem.dart';
 
 class UnirseJuegoScreem extends StatefulWidget {
-  const UnirseJuegoScreem({super.key});
+  const UnirseJuegoScreem({Key? key}) : super(key: key);
+
   static const String nombre = 'UnirseJuegoScreem';
+
   @override
   State<UnirseJuegoScreem> createState() => _UnirseJuegoScreemState();
 }
 
 class _UnirseJuegoScreemState extends State<UnirseJuegoScreem> {
+  late String _searchQuery = '';
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Unirse a un juego ",
-          style: TextStyle(color: Colors.black),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Unirse a un juego",
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.orange,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.orange,
-      ),
-      drawer: Drawer(
-        backgroundColor: Colors.black,
-        shadowColor: Colors.white,
-        child: ListView(padding: EdgeInsets.zero, children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.orange,
+        drawer: Drawer(
+          backgroundColor: Colors.black,
+          shadowColor: Colors.white,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                ),
+                child: Icon(
+                  Icons.person,
+                  size: 100,
+                  color: Colors.black,
+                ),
+              ),
+              ListTile(
+                title:
+                    const Text('Home ', style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(MyHomePage.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Crear equipo ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(CEquipoScreem.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Unirse a Juego ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(UnirseJuegoScreem.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Reservar instalaciones ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(ReservarScreem.nombre);
+                },
+              ),
+              ListTile(
+                title: const Text('Realizar pago ',
+                    style: TextStyle(color: Colors.orange)),
+                onTap: () {
+                  Navigator.of(context).pushNamed(PagoScreem.nombre);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Buscar equipo',
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery = value;
+                });
+              },
             ),
-            child: Icon(
-              Icons.person,
-              size: 100,
-              color: Colors.black,
+            Expanded(
+              child:
+                  _buildTeamList(), // Método para construir la lista de equipos
             ),
-          ),
-          ListTile(
-            title: const Text('Home ', style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(MyHomePage.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Crear equipo ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(CEquipoScreem.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Unirse a Juego ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(UnirseJuegoScreem.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Reservar instalaciones ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () {
-              Navigator.of(context).pushNamed(ReservarScreem.nombre);
-            },
-          ),
-          ListTile(
-            title: const Text('Realizar pago ',
-                style: TextStyle(color: Colors.orange)),
-            onTap: () { Navigator.of(context).pushNamed(PagoScreem.nombre);},
-          ),
-        ]),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+
+  // Método para construir la lista de equipos según el criterio de búsqueda
+  Widget _buildTeamList() {
+    // Aquí puedes implementar la lógica para filtrar los equipos según el criterio de búsqueda
+    // y luego construir una lista de widgets que representen los equipos encontrados.
+    // Por ejemplo:
+    List<Widget> teams =
+        []; // Aquí deberías llenar esta lista con los widgets de los equipos encontrados.
+
+    // Ejemplo de cómo agregar un widget de equipo a la lista:
+    teams.add(
+      ListTile(
+        title: Text('Nombre del equipo'),
+        subtitle: Text('Descripción del equipo'),
+        onTap: () {
+          // Aquí puedes definir qué hacer cuando se seleccione un equipo de la lista.
+        },
+      ),
+    );
+
+    // Devolver la lista de widgets de equipos
+    return ListView(
+      children: teams,
+    );
   }
 }
